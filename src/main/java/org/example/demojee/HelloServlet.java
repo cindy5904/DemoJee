ef36778f3ef366f74d6b199b8a9d661e60cd4995
Cindy Telmon <telcindy59@gmail.com>
2024-06-10 11:37:41 +0200

    Demo JEE

---
+++ src/main/java/org/example/demojee/HelloServlet.java
@@ -0,0 +1,28 @@
+package org.example.demojee;
+
+import java.io.*;
+import jakarta.servlet.http.*;
+import jakarta.servlet.annotation.*;
+
+@WebServlet(name = "helloServlet", value = "/hello-servlet")
+public class HelloServlet extends HttpServlet {
+    private String message;
+
+    public void init() {
+        message = "Hello World!";
+    }
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        response.setContentType("text/html");
+
+        // Hello
+        PrintWriter out = response.getWriter();
+        out.println("<html><body>");
+        out.println("<h1>" + message + " Je suis sur la page HelloServlet</h1>");
+        out.println("</body></html>");
+
+    }
+
+    public void destroy() {
+    }
+}
